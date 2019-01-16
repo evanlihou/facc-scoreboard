ca18d6a2c3bd140280fc3af4b7499e9f025ebdf1
Evan Lihou <git@evanlihou.com>
2019-01-15 19:58:08 -0500

    Minor changes to client which will be removed in next commit

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -4,19 +4,29 @@ function emptyElement(elem) {
   }
 }
 
-var socket = io('http://localhost');
+var settings = {
+  socketServer: document.getElementById('socketServer').value
+};
 
-var eventName = document.getElementById('eventName');
-var matchName = document.getElementById('matchName');
+var socket = io(settings.socketServer);
 
-var redInfo = document.getElementById('redScore');
-var blueInfo = document.getElementById('blueScore');
+var scoreboard = {};
 
-var redTeams = redInfo.getElementsByClassName('teams')[0].children[0];
-var blueTeams = blueInfo.getElementsByClassName('teams')[0].children[0];
+scoreboard.eventName = document.getElementById('eventName');
+scoreboard.matchName = document.getElementById('matchName');
 
-emptyElement(redTeams);
+scoreboard.redInfo = document.getElementById('redScore');
+scoreboard.blueInfo = document.getElementById('blueScore');
 
-var tmp = document.createElement('li');
-tmp.appendChild(document.createTextNode('Team 1'));
-redTeams.appendChild(tmp);
+scoreboard.redInfo.redTeams = scoreboard.redInfo.getElementsByClassName(
+  'teams'
+)[0].children[0];
+scoreboard.blueInfo.blueTeams = scoreboard.blueInfo.getElementsByClassName(
+  'teams'
+)[0].children[0];
+
+// emptyElement(redTeams);
+
+// var tmp = document.createElement('li');
+// tmp.appendChild(document.createTextNode('Team 1'));
+// redTeams.appendChild(tmp);
